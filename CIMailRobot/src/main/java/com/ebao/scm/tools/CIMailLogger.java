a6152c774e0947a67063034bbac96bdc1bcdc42d
foolhunger <foolhunger@gmail.com>
2014-05-28 22:28:58 +0800

    format normalization
    convert all tabs to two spaces, for better code formatting

--- CIMailRobot/src/main/java/com/ebao/scm/tools/CIMailLogger.java
+++ CIMailRobot/src/main/java/com/ebao/scm/tools/CIMailLogger.java
@@ -5,33 +5,33 @@ import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
 public abstract class CIMailLogger {
-	public static PrintWriter stdLoggerFactory() {
-		return stdLogger;
-	}
-	
-	public static PrintWriter errLoggerFactory() {
-		return errLogger;
-	}
-	
-	public abstract void houseKeeping();
-	
-	private static PrintWriter stdLogger;
-	private static PrintWriter errLogger;
-	
-	static {
-		try {
-			stdLogger = new PrintWriter("stdLog.txt", "UTF-8");
-			errLogger = new PrintWriter("errLog.txt", "UTF-8");
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("==> Oops, failed to instantiate logger objects [" + e.getMessage() + "]");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-		catch (UnsupportedEncodingException e) {
-			System.out.println("==> Oops, unsupported encoding for logger objects [" + e.getMessage() + "]");
-			e.printStackTrace();
-			System.exit(-1);
-		}
-	}
+  public static PrintWriter stdLoggerFactory() {
+    return stdLogger;
+  }
+  
+  public static PrintWriter errLoggerFactory() {
+    return errLogger;
+  }
+  
+  public abstract void houseKeeping();
+  
+  private static PrintWriter stdLogger;
+  private static PrintWriter errLogger;
+  
+  static {
+    try {
+      stdLogger = new PrintWriter("stdLog.txt", "UTF-8");
+      errLogger = new PrintWriter("errLog.txt", "UTF-8");
+    }
+    catch (FileNotFoundException e) {
+      System.out.println("==> Oops, failed to instantiate logger objects [" + e.getMessage() + "]");
+      e.printStackTrace();
+      System.exit(-1);
+    }
+    catch (UnsupportedEncodingException e) {
+      System.out.println("==> Oops, unsupported encoding for logger objects [" + e.getMessage() + "]");
+      e.printStackTrace();
+      System.exit(-1);
+    }
+  }
 }
--- CIMailRobot/src/main/java/com/ebao/scm/tools/CIMailParser.java
+++ CIMailRobot/src/main/java/com/ebao/scm/tools/CIMailParser.java
@@ -11,127 +11,127 @@ import java.util.Scanner;
 import org.apache.commons.io.FileUtils;
 
 public final class CIMailParser extends CIMailLogger {
-	public CIMailParser() {
-		compileErrLogs = new ArrayList<String>(50);
-		compileErrDetails = new HashMap<String, String>(50);
-	}
-	
-	public boolean parse(String clog, String prefix) {
-		if (clog == null) {
-			System.out.println("==> Oops, clog cannot be null [CIMailParser.parse()]");
-			errLogger.println("==> Oops, clog cannot be null [CIMailParser.parse()]");
-			houseKeeping();
-			System.exit(-1);
-		}
-		if (prefix == null) {
-			System.out.println("==> Oops, prefix cannot be null [CIMailParser.parse()]");
-			errLogger.println("==> Oops, prefix cannot be null [CIMailParser.parse()]");
-			houseKeeping();
-			System.exit(-1);
-		}
-		compileErrLogs.clear();
-		compileErrDetails.clear();
-		try {
-			clog = clog.replaceAll("\\\\", "/").trim();
-			FileUtils.copyFile(new File(clog), new File("clog.txt"));
-			Scanner sin = new Scanner(new File("clog.txt"));
-			// parse for compilation error logs
-			Pattern ptnBOE = Pattern.compile("(?:\\[(?:javac|info)\\]\\s*compiling\\s+\\d+)|(?:\\[error\\]\\s*failed\\s+to.+compilation\\s+failure)", Pattern.CASE_INSENSITIVE);
-			Pattern ptnEOE = Pattern.compile("(?:\\[(?:javac|info)\\]\\s*\\d+\\s*error)|(?:\\[error\\]\\s*->\\s*\\[\\s*help\\s*\\d+\\s*\\])|(?:\\[(?:java|info)\\]\\s*build\\s+(?:failed|failure))", Pattern.CASE_INSENSITIVE);
-			boolean flag = false;
-			while (sin.hasNextLine()) {
-				String line = sin.nextLine().trim();
-				Matcher mtrBOE = ptnBOE.matcher(line);
-				Matcher mtrEOE = ptnEOE.matcher(line);
-				if (mtrBOE.find()) {
-					compileErrLogs.clear();
-				}
-				compileErrLogs.add(line);
-				if (mtrEOE.find()) {
-					if (!compileErrLogs.isEmpty()) {
-						flag = true;
-					}
-					break;
-				}
-			}
-			if (!flag) {
-				System.out.println("==> No compilation errors have been found [" + clog + "]");
-				stdLogger.println("==> No compilation errors have been found [" + clog + "]");
-				return false;
-			}
-			// parse for compilation error files
-			prefix = prefix.replaceAll("\\\\", "/").trim();
-			Pattern pattern = Pattern.compile(".+?\\/([a-zA-Z]\\$\\/.+?)\\/*\\Z", Pattern.CASE_INSENSITIVE);
-			Matcher matcher = pattern.matcher(prefix);
-			String localPath = "";
-			if (matcher.find()) {
-				localPath = matcher.group(1).replaceFirst("\\$", "\\:");
-				stdLogger.println("==> local path is [" + localPath + "] [CIMailParser.parse()]");
-			}
-			else {
-				System.out.println("==> Oops, path format is incorrent [" + prefix + "] [CIMailParser.parse()]");
-				errLogger.println("==> Oops, path format is incorrent [" + prefix + "] [CIMailParser.parse()]");
-				System.exit(-1);
-			}
-			pattern = Pattern.compile("\\[(?:javac|error)\\].*?\\Q" + localPath + "\\E\\/(.+?)\\s*\\:", Pattern.CASE_INSENSITIVE);
-			flag = false;
-			for (String line: compileErrLogs) {
-				line = line.replaceAll("\\\\", "/").trim();
-				matcher = pattern.matcher(line);
-				if (matcher.find()) {
-					if (!compileErrDetails.containsKey(matcher.group(1))) {
-						System.out.println("==> ##### [" + matcher.group(1) + "]");
-						stdLogger.println("==> ##### [" + matcher.group(1) + "]");
-						compileErrDetails.put(matcher.group(1), "N/A");
-					}
-					flag = true;
-				}
-			}
-			if (!flag) {
-				System.out.println("==> No compilation error files have been found [" + clog + "]");
-				errLogger.println("==> No compilation error files have been found [" + clog + "]");
-				System.exit(-1);
-			}
-			// parse for compilation error details
-			CIMailSVNUtils svnUtils = new CIMailSVNUtils();
-			for (String errFile: compileErrDetails.keySet()) {
-				String svnInfo = svnUtils.getWCLastCommitAuthorRevisionAndMessage(prefix + "/" + errFile);
-				if (svnInfo != null) {
-					compileErrDetails.put(errFile, svnInfo);
-				}
-			}
-			stdLogger.println("--------------------------------------------------");
-			stdLogger.println(compileErrDetails);
-			stdLogger.println("--------------------------------------------------");
-		}
-		catch (IOException e) {
-			System.out.println("==> Oops, failed to copy/read compilation log [" + e.getMessage() + "] [CIMailParser.parse()]");
-			errLogger.println("==> Oops, failed to copy/read compilation log [" + e.getMessage() + "] [CIMailParser.parse()]");
-			e.printStackTrace();
-			houseKeeping();
-			System.exit(-1);
-		}
-		return true;
-	}
-	
-	// parse() must be called before this method, otherwise, the result is undefined
-	public ArrayList<String> getCompileErrLogs() {
-		return compileErrLogs;
-	}
-	
-	// parse() must be called before this method, otherwise, the result is undefined
-	public HashMap<String, String> getCompileErrDetails() {
-		return compileErrDetails;
-	}
-	
-	@Override
-	public void houseKeeping() {
-		stdLogger.close();
-		errLogger.close();
-	}
-	
-	private PrintWriter stdLogger = stdLoggerFactory();
-	private PrintWriter errLogger = errLoggerFactory();
-	private ArrayList<String> compileErrLogs;
-	private HashMap<String, String> compileErrDetails;
+  public CIMailParser() {
+    compileErrLogs = new ArrayList<String>(50);
+    compileErrDetails = new HashMap<String, String>(50);
+  }
+  
+  public boolean parse(String clog, String prefix) {
+    if (clog == null) {
+      System.out.println("==> Oops, clog cannot be null [CIMailParser.parse()]");
+      errLogger.println("==> Oops, clog cannot be null [CIMailParser.parse()]");
+      houseKeeping();
+      System.exit(-1);
+    }
+    if (prefix == null) {
+      System.out.println("==> Oops, prefix cannot be null [CIMailParser.parse()]");
+      errLogger.println("==> Oops, prefix cannot be null [CIMailParser.parse()]");
+      houseKeeping();
+      System.exit(-1);
+    }
+    compileErrLogs.clear();
+    compileErrDetails.clear();
+    try {
+      clog = clog.replaceAll("\\\\", "/").trim();
+      FileUtils.copyFile(new File(clog), new File("clog.txt"));
+      Scanner sin = new Scanner(new File("clog.txt"));
+      // parse for compilation error logs
+      Pattern ptnBOE = Pattern.compile("(?:\\[(?:javac|info)\\]\\s*compiling\\s+\\d+)|(?:\\[error\\]\\s*failed\\s+to.+compilation\\s+failure)", Pattern.CASE_INSENSITIVE);
+      Pattern ptnEOE = Pattern.compile("(?:\\[(?:javac|info)\\]\\s*\\d+\\s*error)|(?:\\[error\\]\\s*->\\s*\\[\\s*help\\s*\\d+\\s*\\])|(?:\\[(?:java|info)\\]\\s*build\\s+(?:failed|failure))", Pattern.CASE_INSENSITIVE);
+      boolean flag = false;
+      while (sin.hasNextLine()) {
+        String line = sin.nextLine().trim();
+        Matcher mtrBOE = ptnBOE.matcher(line);
+        Matcher mtrEOE = ptnEOE.matcher(line);
+        if (mtrBOE.find()) {
+          compileErrLogs.clear();
+        }
+        compileErrLogs.add(line);
+        if (mtrEOE.find()) {
+          if (!compileErrLogs.isEmpty()) {
+            flag = true;
+          }
+          break;
+        }
+      }
+      if (!flag) {
+        System.out.println("==> No compilation errors have been found [" + clog + "]");
+        stdLogger.println("==> No compilation errors have been found [" + clog + "]");
+        return false;
+      }
+      // parse for compilation error files
+      prefix = prefix.replaceAll("\\\\", "/").trim();
+      Pattern pattern = Pattern.compile(".+?\\/([a-zA-Z]\\$\\/.+?)\\/*\\Z", Pattern.CASE_INSENSITIVE);
+      Matcher matcher = pattern.matcher(prefix);
+      String localPath = "";
+      if (matcher.find()) {
+        localPath = matcher.group(1).replaceFirst("\\$", "\\:");
+        stdLogger.println("==> local path is [" + localPath + "] [CIMailParser.parse()]");
+      }
+      else {
+        System.out.println("==> Oops, path format is incorrent [" + prefix + "] [CIMailParser.parse()]");
+        errLogger.println("==> Oops, path format is incorrent [" + prefix + "] [CIMailParser.parse()]");
+        System.exit(-1);
+      }
+      pattern = Pattern.compile("\\[(?:javac|error)\\].*?\\Q" + localPath + "\\E\\/(.+?)\\s*\\:", Pattern.CASE_INSENSITIVE);
+      flag = false;
+      for (String line: compileErrLogs) {
+        line = line.replaceAll("\\\\", "/").trim();
+        matcher = pattern.matcher(line);
+        if (matcher.find()) {
+          if (!compileErrDetails.containsKey(matcher.group(1))) {
+            System.out.println("==> ##### [" + matcher.group(1) + "]");
+            stdLogger.println("==> ##### [" + matcher.group(1) + "]");
+            compileErrDetails.put(matcher.group(1), "N/A");
+          }
+          flag = true;
+        }
+      }
+      if (!flag) {
+        System.out.println("==> No compilation error files have been found [" + clog + "]");
+        errLogger.println("==> No compilation error files have been found [" + clog + "]");
+        System.exit(-1);
+      }
+      // parse for compilation error details
+      CIMailSVNUtils svnUtils = new CIMailSVNUtils();
+      for (String errFile: compileErrDetails.keySet()) {
+        String svnInfo = svnUtils.getWCLastCommitAuthorRevisionAndMessage(prefix + "/" + errFile);
+        if (svnInfo != null) {
+          compileErrDetails.put(errFile, svnInfo);
+        }
+      }
+      stdLogger.println("--------------------------------------------------");
+      stdLogger.println(compileErrDetails);
+      stdLogger.println("--------------------------------------------------");
+    }
+    catch (IOException e) {
+      System.out.println("==> Oops, failed to copy/read compilation log [" + e.getMessage() + "] [CIMailParser.parse()]");
+      errLogger.println("==> Oops, failed to copy/read compilation log [" + e.getMessage() + "] [CIMailParser.parse()]");
+      e.printStackTrace();
+      houseKeeping();
+      System.exit(-1);
+    }
+    return true;
+  }
+  
+  // parse() must be called before this method, otherwise, the result is undefined
+  public ArrayList<String> getCompileErrLogs() {
+    return compileErrLogs;
+  }
+  
+  // parse() must be called before this method, otherwise, the result is undefined
+  public HashMap<String, String> getCompileErrDetails() {
+    return compileErrDetails;
+  }
+  
+  @Override
+  public void houseKeeping() {
+    stdLogger.close();
+    errLogger.close();
+  }
+  
+  private PrintWriter stdLogger = stdLoggerFactory();
+  private PrintWriter errLogger = errLoggerFactory();
+  private ArrayList<String> compileErrLogs;
+  private HashMap<String, String> compileErrDetails;
 }
